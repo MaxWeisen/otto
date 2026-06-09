4c416da8dc49d533477834f8292628417d32caea
Max <germanbluemax@gmail.com>
2026-06-09 14:33:42 -0700

    feat(config): load config from environment variables

--- backend/cmd/otto/main.go
+++ backend/cmd/otto/main.go
@@ -9,29 +9,21 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/maxweisen/otto/backend/internal/config"
 )
-
-func requestLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
-		start := time.Now()
-
-		next.ServeHTTP(ww, r)
-
-		slog.Info("request",
-			"method", r.Method,
-			"path", r.URL.Path,
-			"status", ww.Status(),
-			"duration_ms", time.Since(start).Milliseconds(),
-			"request_id", middleware.GetReqID(r.Context()),
-		)
-	})
-}
 
 func main() {
 	// setup default slog JSON handler
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
+
+	// load environment variables
+	cfg, err := config.Load()
+
+	if err != nil {
+		slog.Error("failed to load environment variables", "err", err)
+		os.Exit(1)
+	}
 
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
@@ -50,10 +42,27 @@ func main() {
 		_, _ = w.Write(res)
 	})
 
-	err := http.ListenAndServe(":3333", r)
+	err = http.ListenAndServe(":"+cfg.Port, r)
 
 	if err != nil {
 		slog.Error("could not start server", "err", err)
 		os.Exit(1)
 	}
+}
+
+func requestLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		start := time.Now()
+
+		next.ServeHTTP(ww, r)
+
+		slog.Info("request",
+			"method", r.Method,
+			"path", r.URL.Path,
+			"status", ww.Status(),
+			"duration_ms", time.Since(start).Milliseconds(),
+			"request_id", middleware.GetReqID(r.Context()),
+		)
+	})
 }
---
+++ backend/internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DB   DatabaseConfig
+	Port string
+}
+
+type DatabaseConfig struct {
+	DatabaseURL string
+	Port        string
+}
+
+func Load() (*Config, error) {
+	err := godotenv.Load()
+
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return nil, err
+	}
+
+	cfg := &Config{
+		Port: getEnv("PORT", "3333"),
+		DB: DatabaseConfig{
+			DatabaseURL: os.Getenv("DB_URL"),
+			Port:        os.Getenv("DB_PORT"),
+		},
+	}
+	return cfg, nil
+}
+
+func getEnv(key, fallback string) string {
+	value, isEnvVariableSet := os.LookupEnv(key)
+
+	if isEnvVariableSet && value != "" {
+		return value
+	}
+	return fallback
+}
